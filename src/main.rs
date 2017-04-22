6127a57da89a02647715e5ff7e127e8044ba2dc7
Tommaso Checchi <tommaso.checchi1@gmail.com>
2017-04-21 21:37:21 -0700

    Trying to push files from the client to the server

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 extern crate clap;
 extern crate byteorder;
 extern crate app_dirs;
+extern crate serde_json;
+extern crate base64;
+extern crate tiny_keccak;
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
 
+use std::fs;
+use std::fs::DirEntry;
 use std::process::Command;
 use std::process::Stdio;
 use std::io::prelude::*;
@@ -16,12 +24,14 @@ use std::sync::mpsc::channel;
 use std::io::Result;
 use app_dirs::*;
 
+
 const APP_INFO: AppInfo = AppInfo {
     name: "remote_python",
     author: "tommaso",
 };
 
 fn send_request(socket: &mut Write, pathstr: &str) {
+
     let path = Path::new(pathstr);
 
     if let Ok(mut file) = File::open(path) {
@@ -81,16 +91,124 @@ fn handle_output<T: Read + Send + 'static>(stream: T, tx: std::sync::mpsc::Sende
                   });
 }
 
-fn receive_request(socket: &mut TcpStream) {
+fn is_hidden(path: &Path) -> bool {
+    path.file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .starts_with(".")
+}
+
+fn visit_dirs(dir: &Path, cb: &mut FnMut(&DirEntry)) -> std::io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            if is_hidden(&path) {
+                continue;
+            }
+
+            if path.is_dir() {
+                visit_dirs(&path, cb)?;
+            } else {
+                cb(&entry);
+            }
+        }
+    }
+    Ok(())
+}
+
+fn hash_file(path: &Path) -> std::io::Result<String> {
+    println!("{:?}", path);
+
+    let mut file = File::open(&path)?;
+
+    let mut buffer = [0; 4096];
+    let mut hasher = tiny_keccak::Keccak::new_sha3_256();
+    while file.read(&mut buffer)? > 0 {
+        hasher.update(&buffer);
+    }
+
+    let mut res = [0; 32];
+    hasher.finalize(&mut res);
+
+    Ok(base64::encode(&res))
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FolderEntry {
+    path: String,
+    hash: String,
+}
+
+impl FolderEntry {
+    fn from_path(path: &Path) -> std::io::Result<Self> {
+        Ok(FolderEntry {
+               path: path.to_str().unwrap().to_owned(),
+               hash: hash_file(path)?,
+           })
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FolderSignature {
+    entries: Vec<FolderEntry>,
+}
+
+impl FolderSignature {
+    fn new() -> Self {
+        FolderSignature { entries: vec![] }
+    }
+
+    fn add_entry(&mut self, path: &Path) -> std::io::Result<()> {
+        self.entries.push(FolderEntry::from_path(path)?);
+        Ok(())
+    }
+}
+
+fn create_sig_files(cwd: &Path) -> FolderSignature {
+    //make a sig file for each file in the current directory
+    //TODO keep the sigs, only redo them if the timestamp of the file is newer
+    let mut tmp_dir = std::env::temp_dir();
+    tmp_dir.push("remote_python");
+    fs::create_dir_all(&tmp_dir).unwrap();
+
+    let mut sig = FolderSignature::new();
+
+    visit_dirs(&cwd,
+               &mut |entry| { sig.add_entry(&entry.path()).unwrap(); })
+            .unwrap();
+
+    sig
+}
+
+fn receive_request(socket: &mut TcpStream) -> std::io::Result<()> {
+
+    //create or open the cwd
+    let cwd = app_dir(AppDataType::UserConfig, &APP_INFO, "").unwrap();
+
+    //create all sig_files in a given folder
+    let sig = create_sig_files(&cwd);
+
+    //send the request over to the client so it can send new data
+    serde_json::to_writer(socket.try_clone()?, &sig)?;
+
+    //pack and send over all of them
+
+    //receive deltas
+
+    //apply all deltas
+
+    //run python
+
+
     //read how many bytes it will be
-    let len = socket.read_u64::<LittleEndian>().unwrap();
+    let len = socket.read_u64::<LittleEndian>()?;
 
     //read the bytes
     let mut code = String::with_capacity(len as usize);
-    socket.take(len).read_to_string(&mut code).unwrap();
-
-    //create the cwd
-    let cwd = app_dir(AppDataType::UserConfig, &APP_INFO, "").unwrap();
+    socket.take(len).read_to_string(&mut code)?;
 
     //run python
     let child = Command::new(find_python_version())
@@ -98,10 +216,9 @@ fn receive_request(socket: &mut TcpStream) {
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .current_dir(&cwd)
-        .spawn()
-        .unwrap();
+        .spawn()?;
 
-    socket.write(b"Python execution launched\n").unwrap();
+    socket.write(b"Python execution launched\n")?;
 
     let (tx, rx) = channel();
 
@@ -120,6 +237,8 @@ fn receive_request(socket: &mut TcpStream) {
             Err(_) => break,
         }
     }
+
+    Ok(())
 }
 
 fn main() {
@@ -150,6 +269,17 @@ fn main() {
     let address = format!("{}:{}", address, port);
 
     if matches.is_present("server") {
+
+        //create or open the cwd
+        let cwd = app_dir(AppDataType::UserConfig, &APP_INFO, "").unwrap();
+
+        //create all sig_files in a given folder
+        let sig = create_sig_files(&cwd);
+
+        let sig_json = serde_json::to_string(&sig).unwrap();
+
+        println!("{}", sig_json);
+
         println!("Opening server at {}", address);
 
         let listener = TcpListener::bind(address).unwrap();
@@ -158,7 +288,9 @@ fn main() {
             println!("Handling request");
             match stream {
                 Ok(mut stream) => {
-                    receive_request(&mut stream);
+                    if let Err(e) = receive_request(&mut stream) {
+                        println!("Something went wrong: {:?}", e);
+                    }
                 }
                 Err(_) => { /* connection failed */ }
             }
